9f3d16e427351bd747364a278b39b85d33426272
Ahmad Ridwan <ahmadridwan.3107@gmail.com>
2024-08-17 14:18:29 +0700

    chore: add runtime config and nuxt image

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,11 +2,16 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  modules: ["@pinia/nuxt"],
+  modules: ["@pinia/nuxt", "@nuxt/image"],
   postcss: {
     plugins: {
       autoprefixer: {},
     },
   },
   css: ["~/assets/css/main.css"],
+  runtimeConfig: {
+    public: {
+      apiBase: process.env.NUXT_PUBLIC_API_BASE, // can be overridden by NUXT_PUBLIC_API_BASE environment variable
+    },
+  },
 });
