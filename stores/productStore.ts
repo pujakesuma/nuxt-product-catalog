cf8922a36af4a7db41b92106eaf12c50b3950d76
Ahmad Ridwan <ahmadridwan.3107@gmail.com>
2024-08-17 14:20:13 +0700

    feat: update store

--- stores/productStore.ts
+++ stores/productStore.ts
@@ -1,19 +1,32 @@
-import { defineStore } from 'pinia'
-import type { Product } from '~/types/Product'
+import { defineStore } from "pinia";
+import type { Product } from "~/types/Product";
+import type { AsyncDataRequestStatus } from "#app";
 
-export const useProductStore = defineStore('productStore', {
+export const useProductStore = defineStore("productStore", {
   state: () => ({
     products: [] as Product[],
     product: null as Product | null,
+    status: "idle" as AsyncDataRequestStatus,
   }),
+  getters: {
+    isLoading: (state) => state.status === "pending",
+  },
   actions: {
     async fetchProducts() {
-      const products = await $fetch<Product[]>('https://my-json-server.typicode.com/pujakesuma/nuxt-product-catalog/products')
-      this.products = products || []
+      const apiBase = useRuntimeConfig().public.apiBase;
+      const { data, status } = await useAsyncData("products", () =>
+        $fetch<Product[]>(`${apiBase}/products`)
+      );
+      this.products = data.value || [];
+      this.status = status.value;
     },
     async fetchProduct(id: number) {
-      const product = await $fetch<Product>(`https://my-json-server.typicode.com/pujakesuma/nuxt-product-catalog/products/${id}`)
-      this.product = product || null
+      const apiBase = useRuntimeConfig().public.apiBase;
+      const { data, status } = await useAsyncData("product", () =>
+        $fetch<Product>(`${apiBase}/products/${id}`)
+      );
+      this.product = data.value || null;
+      this.status = status.value;
     },
   },
-})
+});
